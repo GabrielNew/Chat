6a3dc43d92d1597a5b995445b668bb35c3f7efc3
GabrielNew <gabrielfai70@gmail.com>
2025-02-15 13:00:00 -0300

    Connection V1

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import http from "http";
+import { Server } from "socket.io";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("new websocket connection");
+});
+
+server.listen(port, () => {
   console.log("The server is online on port: " + port);
 });
